9acbd4ff72902f4004ae697634efb8c60b54231c
roylee0704 <roylee0704@gmail.com>
2016-03-21 16:23:14 +0800

    refactored codebase struct

--- endpoint.go
+++ endpoint.go
@@ -1,18 +1,18 @@
-package naive
+package mcre
 
 import (
 	"github.com/go-kit/kit/endpoint"
 	"golang.org/x/net/context"
 )
 
-// MakeEndpoint creates an Endpoint from mcre service, the endpoint
+// MakePutObjectEndpoint creates an Endpoint from mcre service, the endpoint
 // access to background context, asserts input as PutObjectInput, calls
 // PutObject API and returns PutObjectOutput.
 // TODO: add tests for Etag generation.
-func MakeEndpoint(mcre *naive) endpoint.Endpoint {
+func MakePutObjectEndpoint(s Service) endpoint.Endpoint {
 	return func(ctx context.Context, input interface{}) (interface{}, error) {
 		in := input.(PutObjectInput)
-		etag, err := mcre.PutObject(in.Bucket, in.Key, in.Body)
+		etag, err := s.PutObject(in.Bucket, in.Key, in.Body)
 		if err != nil {
 			return nil, err // this returns error http response, how do u want it? TODO: study this.
 		}
--- payload.go
+++ payload.go
@@ -1,4 +1,4 @@
-package naive
+package mcre
 
 import "io"
 
