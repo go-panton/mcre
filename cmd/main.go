e5bfd2be419f703e98ce7257d8c5b35f8eafe8f9
roylee0704 <roylee0704@gmail.com>
2016-03-22 14:11:29 +0800

    setup microservice to run

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+
+	"golang.org/x/net/context"
+
+	"github.com/go-panton/mcre/download/v1"
+)
 
 func main() {
-	fmt.Println("this is main program")
+
+	ds := download.NewService()
+	ctx := context.Background()
+
+	mux := http.NewServeMux()
+	mux.Handle("/download/v1/", download.MakeHandler(ctx, ds))
+
+	http.Handle("/", mux)
+	log.Fatal(http.ListenAndServe(":8282", nil))
 }
+
+// type server struct{}
+//
+// func (s server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+// 	fmt.Fprintln(w, "woi")
+// }
