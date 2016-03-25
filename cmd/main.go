f7abfc981497839c0168486aab85b28cd8f0e7bc
Rlexsu <alex.teoh.91@gmail.com>
2016-03-25 13:48:57 +0900

    change the directory from user->users

--- cmd/main.go
+++ cmd/main.go
@@ -7,18 +7,18 @@ import (
 	"golang.org/x/net/context"
 
 	"github.com/go-panton/mcre/download/v1"
-	"github.com/go-panton/mcre/user/v1"
+	"github.com/go-panton/mcre/users/v1"
 )
 
 func main() {
 
 	ds := download.NewService()
-	us := user.NewService()
+	us := users.NewService()
 	ctx := context.Background()
 
 	mux := http.NewServeMux()
 	mux.Handle("/download/v1/", download.MakeHandler(ctx, ds))
-	mux.Handle("/user/v1", user.MakeHandler(ctx, us))
+	mux.Handle("/users/v1", users.MakeHandler(ctx, us))
 
 	http.Handle("/", mux)
 	log.Fatal(http.ListenAndServe(":8282", nil))
