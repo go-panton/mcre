8f43dd6d0710b37a431d421620871eb8e5cbed99
roylee0704 <roylee0704@gmail.com>
2016-03-25 10:38:48 +0800

    codereview: microservice ends with

---
+++ users/v1/service.go
@@ -0,0 +1,22 @@
+package user
+
+import "errors"
+
+// Service is the interface of User API
+type Service interface {
+	User(username, password string) (bool, error)
+}
+
+// NewService instantiates new user-service.
+func NewService() Service {
+	return &service{}
+}
+
+type service struct{}
+
+func (svc *service) User(username, password string) (bool, error) {
+	if username != "" && password != "" {
+		return true, nil
+	}
+	return false, errors.New("Username is : " + username + " Password is: " + password)
+}
---
+++ users/v1/transport.go
@@ -0,0 +1,46 @@
+package user
+
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"golang.org/x/net/context"
+
+	kithttp "github.com/go-kit/kit/transport/http"
+)
+
+//MakeHandler create a func to handle that url
+func MakeHandler(ctx context.Context, svc Service) http.Handler {
+	signUpHandler := kithttp.NewServer(
+		ctx,
+		makeSignUpEndPoint(svc),
+		decodeSignUpRequest,
+		encodeSignUpResponse,
+	)
+
+	r := mux.NewRouter()
+	r.Handle("/user/v1", signUpHandler).Methods("POST")
+
+	return r
+}
+
+func decodeSignUpRequest(r *http.Request) (interface{}, error) {
+	var req SignUpRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		return nil, err
+	}
+
+	return SignUpRequest{req.Username, req.Password}, nil
+}
+
+func encodeSignUpResponse(w http.ResponseWriter, response interface{}) error {
+	resp := response.(SignUpResponse)
+	respb, err := json.Marshal(resp)
+	if err != nil {
+		return errors.New("Unable to marshal repsonse into json")
+	}
+	w.Write(respb)
+	return nil
+}
