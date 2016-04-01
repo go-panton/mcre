720bd1270c8ae82d2fdfe353fa80efe833fcd52d
roylee0704 <roylee0704@gmail.com>
2016-04-01 17:40:49 +0800

    initial commit infra for gokit

---
+++ infrastructure/deliver/gokit/files/endpoint.go
@@ -0,0 +1,15 @@
+package files
+
+import (
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-panton/mcre/files"
+	"golang.org/x/net/context"
+)
+
+func makeDownloadEndpoint(svc files.Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (interface{}, error) {
+		req := request.(files.DownloadRequest)
+		resp := svc.Download(req.FileId)
+		return files.DownloadResponse{FileContent: resp}, nil
+	}
+}
---
+++ infrastructure/deliver/gokit/files/server_test.go
@@ -0,0 +1,26 @@
+package files
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/go-panton/mcre/files"
+
+	"golang.org/x/net/context"
+)
+
+func TestIntegration(t *testing.T) {
+	server := MakeHandler(context.Background(), files.NewService())
+
+	w := httptest.NewRecorder()
+	r, err := http.NewRequest("GET", "/files/123", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.ServeHTTP(w, r)
+
+	fmt.Println(w.Body.String())
+}
