7a37c61c2971eb41328ad711e0380649d6e12f30
roylee0704 <roylee0704@gmail.com>
2016-04-02 17:56:55 +0800

    update to reflect new changes

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,38 @@ import (
 	"log"
 	"net/http"
 
-	"github.com/go-panton/mcre/infrastructure/gokit"
+	"golang.org/x/net/context"
+
+	"github.com/go-panton/mcre/files"
+	"github.com/go-panton/mcre/infra/gokit/fileserver"
+	"github.com/go-panton/mcre/infra/gokit/userserver"
+	"github.com/go-panton/mcre/infra/store/mysql"
+	"github.com/go-panton/mcre/users"
+	"github.com/gorilla/mux"
 )
 
 var (
-	port = flag.String("port", ":8282", "Listen port")
+	port   = flag.String("port", ":8282", "Listen port")
+	router = mux.NewRouter()
+	mcrev1 = router.PathPrefix("/mcre/v1").Subrouter()
 )
 
 func main() {
 	flag.Parse()
 
-	http.Handle("/", gokit.NewKit())
+	//mongoDbName := "go_panton"
+	//mongoColName := "user"
+	mysqlconnectionString := "root:root123@/go_panton"
+
+	//us := users.NewService(mongo.NewUser(mongo.ConnectDatabase(mongoDbName, mongoColName)))
+
+	us := users.NewService(mysql.NewUser(mysql.ConnectDatabase(mysqlconnectionString)))
+	usersvr := userserver.NewServer(context.Background(), us)
+	usersvr.RouteTo(mcrev1)
+
+	filesvr := fileserver.NewServer(context.Background(), files.NewService())
+	filesvr.RouteTo(mcrev1)
+
+	http.Handle("/", router)
 	log.Fatal(http.ListenAndServe(*port, nil))
 }
