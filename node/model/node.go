d05179cbc1c4776be0285cf288150c2c417e5830
Rlexsu <alex.teoh.91@gmail.com>
2016-04-19 13:35:47 +0900

    Initial commit for node model

---
+++ node/model/node.go
@@ -0,0 +1,28 @@
+package model
+
+type Node struct {
+	NodeID    int
+	NodeBits  int
+	NodeDesc  string
+	NodeDT    string //t.format("2006-01-02")
+	NodeGID   int
+	NodeHash  string
+	NodeLevel int
+	NodeType  string
+	NodeUID   int
+}
+
+var insertString string
+var updateString string
+var deleteString string
+
+type NodeRepository interface {
+	Create(Node) error
+	Update(Node) error
+	Delete(int) error
+	Find(int) (Node, error)
+	FindByDesc(string) ([]Node, error)
+	ConstructInsertString(Node) (string, error)
+	ConstructUpdateString(Node) (string, error)
+	ConstructDeleteString(int) (string, error)
+}
