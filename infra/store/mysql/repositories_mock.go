3ad14f80618979d6794474bbefab65f2231df799
Rlexsu <alex.teoh.91@gmail.com>
2016-04-25 18:00:18 +0900

    Added mock methods implementation for node,nodelink,fmedia,fverinfo

--- infra/store/mysql/repositories_mock.go
+++ infra/store/mysql/repositories_mock.go
@@ -4,20 +4,23 @@ import (
 	"errors"
 	"strconv"
 
+	fmedias "github.com/go-panton/mcre/fmedias/models"
 	nodes "github.com/go-panton/mcre/nodes/models"
 	seqs "github.com/go-panton/mcre/seqs/models"
 	users "github.com/go-panton/mcre/users/models"
 )
 
-type mockUserRepository struct {
-	UserArray []users.User
-}
+type mockUserRepository struct{}
 
 type mockSeqRepository struct{}
 
-type mockNodeRepository struct {
-	node nodes.Node
-}
+type mockNodeRepository struct{}
+
+type mockFmediaRepository struct{}
+
+type mockFverinfoRepository struct{}
+
+type mockNodelinkRepository struct{}
 
 //NewMockUserRepository return a mock UserRepository for testing purpose
 func NewMockUserRepository() users.UserRepository {
@@ -32,6 +35,21 @@ func NewMockSeqRepository() seqs.SeqRepository {
 //NewMockNodeRepository return a mock NodeRepository for testing purpose
 func NewMockNodeRepository() nodes.NodeRepository {
 	return &mockNodeRepository{}
+}
+
+//NewMockFmediaRepository return a mock FmediaRepository for testing purpose
+func NewMockFmediaRepository() fmedias.FmediaRepository {
+	return &mockFmediaRepository{}
+}
+
+//NewMockFverinfoRepository return a mock FverinfoRepository for testing purpose
+func NewMockFverinfoRepository() fmedias.FverinfoRepository {
+	return &mockFverinfoRepository{}
+}
+
+//NewMockNodelinkRepository return a mock NodelinkRepository for testing purpose
+func NewMockNodelinkRepository() nodes.NodelinkRepository {
+	return &mockNodelinkRepository{}
 }
 
 func (m *mockUserRepository) Find(username string) (*users.User, error) {
@@ -127,7 +145,7 @@ func (m *mockNodeRepository) GetInsertStr(node nodes.Node) (string, error) {
 }
 
 func (m *mockNodeRepository) GetUpdateStr(node nodes.Node) (string, error) {
-	if node.NodeID == 0 || node.NodeDesc == "" || node.NodeGID == 0 || node.NodeUID == 0 {
+	if node.NodeID == 0 || node.NodeDesc == "" || node.NodeGID == 0 || node.NodeUID == 0 || node.NodeType == "" {
 		return "", errors.New("Parameter cannot be empty")
 	}
 	updateStr := "UPDATE node SET nodedesc=" + node.NodeDesc +
@@ -145,6 +163,206 @@ func (m *mockNodeRepository) GetDeleteStr(nodeID int) (string, error) {
 	}
 
 	deleteStr := "DELETE FROM node WHERE nodeid=" + strconv.Itoa(nodeID)
+
+	return deleteStr, nil
+}
+
+func (m *mockFmediaRepository) Insert(fmedia fmedias.Fmedia) error {
+	if fmedia.NodeID == 0 || fmedia.FDesc == "" || fmedia.FExt == "" || fmedia.FFulPath == "" || fmedia.FGName == "" || fmedia.FOName == "" || fmedia.FSize == 0 {
+		return errors.New("Paramater cannot be empty")
+	}
+	return nil
+}
+
+func (m *mockFmediaRepository) Update(fmedia fmedias.Fmedia) error {
+	if fmedia.NodeID == 0 || fmedia.FDesc == "" || fmedia.FExt == "" || fmedia.FFulPath == "" || fmedia.FGName == "" || fmedia.FOName == "" || fmedia.FSize == 0 {
+		return errors.New("Paramater cannot be empty")
+	}
+	return nil
+}
+
+func (m *mockFmediaRepository) Delete(nodeID int) error {
+	if nodeID == 0 {
+		return errors.New("Node ID cannot be empty or 0")
+	}
+	return nil
+}
+func (m *mockFmediaRepository) Find(nodeID int) (fmedias.Fmedia, error) {
+	if nodeID == 0 {
+		return fmedias.Fmedia{}, errors.New("Node ID cannot be empty or 0")
+	}
+	return fmedias.Fmedia{}, nil
+}
+
+func (m *mockFmediaRepository) FindByFileDesc(fileDesc string) ([]fmedias.Fmedia, error) {
+	if fileDesc == "" {
+		return []fmedias.Fmedia{}, errors.New("File description cannot be empty")
+	}
+	return []fmedias.Fmedia{}, nil
+}
+
+func (m *mockFmediaRepository) GetInsertStr(fmedia fmedias.Fmedia) (string, error) {
+	if fmedia.NodeID == 0 || fmedia.FDesc == "" || fmedia.FExt == "" || fmedia.FFulPath == "" || fmedia.FGName == "" || fmedia.FOName == "" || fmedia.FSize == 0 {
+		return "", errors.New("Paramater cannot be empty")
+	}
+	insertStr := "INSERT fmedia SET nodeid=" + strconv.Itoa(fmedia.NodeID) +
+		",fdesc=" + fmedia.FDesc +
+		",fext=" + fmedia.FExt +
+		",ffulpath=" + fmedia.FFulPath +
+		",fgname=" + fmedia.FGName +
+		",foname=" + fmedia.FOName +
+		",fremark=" + fmedia.FRemark +
+		",fsize=" + strconv.Itoa(fmedia.FSize) +
+		",fstatus=" + strconv.Itoa(fmedia.FStatus) +
+		",ftype=" + strconv.Itoa(fmedia.FType)
+
+	return insertStr, nil
+}
+
+func (m *mockFmediaRepository) GetUpdateStr(fmedia fmedias.Fmedia) (string, error) {
+	if fmedia.NodeID == 0 || fmedia.FDesc == "" || fmedia.FExt == "" || fmedia.FFulPath == "" || fmedia.FGName == "" || fmedia.FOName == "" || fmedia.FSize == 0 {
+		return "", errors.New("Paramater cannot be empty")
+	}
+	updateStr := "UPDATE fmedia SET fdesc=" + fmedia.FDesc +
+		",fext=" + fmedia.FExt +
+		",ffulpath=" + fmedia.FFulPath +
+		",fgname=" + fmedia.FGName +
+		",foname=" + fmedia.FOName +
+		",fremark=" + fmedia.FRemark +
+		",fsize=" + strconv.Itoa(fmedia.FSize) +
+		",fstatus=" + strconv.Itoa(fmedia.FStatus) +
+		",ftype=" + strconv.Itoa(fmedia.FType) +
+		" WHERE nodeid = " + strconv.Itoa(fmedia.NodeID)
+
+	return updateStr, nil
+}
+
+func (m *mockFmediaRepository) GetDeleteStr(nodeID int) (string, error) {
+	if nodeID == 0 {
+		return "", errors.New("Node ID cannot be empty or 0")
+	}
+	deleteStr := "DELETE FROM fmedia WHERE nodeid=" + strconv.Itoa(nodeID)
+
+	return deleteStr, nil
+}
+
+func (m *mockFverinfoRepository) Insert(fverinfo fmedias.Fverinfo) error {
+	if fverinfo.NodeID == 0 || fverinfo.StartDate == "" || fverinfo.Version == "" || fverinfo.VerState == 0 {
+		return errors.New("Parameter cannot be empty")
+	}
+	return nil
+}
+
+func (m *mockFverinfoRepository) Update(fverinfo fmedias.Fverinfo) error {
+	if fverinfo.NodeID == 0 || fverinfo.StartDate == "" || fverinfo.Version == "" || fverinfo.VerState == 0 {
+		return errors.New("Parameter cannot be empty")
+	}
+	return nil
+}
+
+func (m *mockFverinfoRepository) Delete(nodeID int) error {
+	if nodeID == 0 {
+		return errors.New("Node ID cannot be empty or 0")
+	}
+	return nil
+}
+
+func (m *mockFverinfoRepository) Find(nodeID int) (fmedias.Fverinfo, error) {
+	if nodeID == 0 {
+		return fmedias.Fverinfo{}, errors.New("Node ID cannot be empty or 0")
+	}
+	return fmedias.Fverinfo{}, nil
+}
+
+func (m *mockFverinfoRepository) GetInsertStr(fverinfo fmedias.Fverinfo) (string, error) {
+	if fverinfo.NodeID == 0 || fverinfo.StartDate == "" || fverinfo.Version == "" || fverinfo.VerState == 0 {
+		return "", errors.New("Parameter cannot be empty")
+	}
+	insertStr := "INSERT fverinfo SET nodeid=" + strconv.Itoa(fverinfo.NodeID) +
+		",enddate=" + fverinfo.EndDate +
+		",remarks=" + fverinfo.Remarks +
+		",startdate=" + fverinfo.StartDate +
+		",version=" + fverinfo.Version +
+		",verstate=" + strconv.Itoa(fverinfo.VerState)
+
+	return insertStr, nil
+}
+
+func (m *mockFverinfoRepository) GetUpdateStr(fverinfo fmedias.Fverinfo) (string, error) {
+	if fverinfo.NodeID == 0 || fverinfo.StartDate == "" || fverinfo.Version == "" || fverinfo.VerState == 0 {
+		return "", errors.New("Parameter cannot be empty")
+	}
+	updateStr := "UPDATE fverinfo SET enddate=" + fverinfo.EndDate +
+		",remarks=" + fverinfo.Remarks +
+		",startdate=" + fverinfo.StartDate +
+		",version=" + fverinfo.Version +
+		",verstate=" + strconv.Itoa(fverinfo.VerState) +
+		" WHERE nodeid=" + strconv.Itoa(fverinfo.NodeID)
+
+	return updateStr, nil
+}
+
+func (m *mockFverinfoRepository) GetDeleteStr(nodeID int) (string, error) {
+	if nodeID == 0 {
+		return "", errors.New("Node ID cannot be empty or 0")
+	}
+	deleteStr := "DELETE FROM fverinfo WHERE nodeid=" + strconv.Itoa(nodeID)
+
+	return deleteStr, nil
+}
+
+func (m *mockNodelinkRepository) Insert(nl nodes.Nodelink) error {
+	if nl.LinkCNodeID == 0 || nl.LinkPNodeID == 0 || nl.LinkType == "" {
+		return errors.New("Parameter cannot be empty")
+	}
+	return nil
+}
+
+func (m *mockNodelinkRepository) Delete(childNodeID, parentNodeID int) error {
+	if childNodeID == 0 || parentNodeID == 0 {
+		return errors.New("Parameter cannot be empty")
+	}
+	return nil
+}
+
+func (m *mockNodelinkRepository) FindByChild(childNodeID int) ([]int, error) {
+	if childNodeID == 0 {
+		return []int{}, errors.New("Child Node ID cannot be empty or 0")
+	}
+	return []int{}, nil
+}
+
+func (m *mockNodelinkRepository) FindByParent(parentNodeID int) ([]int, error) {
+	if parentNodeID == 0 {
+		return []int{}, errors.New("Parent Node ID cannot be empty or 0")
+	}
+	return []int{}, nil
+}
+
+func (m *mockNodelinkRepository) FindExact(childNodeID int, parentNodeID int, linkType string) (nodes.Nodelink, error) {
+	if childNodeID == 0 || parentNodeID == 0 || linkType == "" {
+		return nodes.Nodelink{}, errors.New("Parameter cannot be empty")
+	}
+	return nodes.Nodelink{}, nil
+}
+
+func (m *mockNodelinkRepository) GetInsertStr(nl nodes.Nodelink) (string, error) {
+	if nl.LinkCNodeID == 0 || nl.LinkPNodeID == 0 || nl.LinkType == "" {
+		return "", errors.New("Parameter cannot be empty")
+	}
+	insertStr := "INSERT nodelink SET linkcnodeid=" + strconv.Itoa(nl.LinkCNodeID) +
+		",linkpnodeid=" + strconv.Itoa(nl.LinkPNodeID) +
+		",linktype=" + nl.LinkType
+
+	return insertStr, nil
+}
+
+func (m *mockNodelinkRepository) GetDeleteStr(childNodeID, parentNodeID int) (string, error) {
+	if childNodeID == 0 || parentNodeID == 0 {
+		return "", errors.New("Parameter cannot be empty")
+	}
+	deleteStr := "DELETE FROM nodelink WHERE linkcnodeid=" + strconv.Itoa(childNodeID) +
+		",linkpnodeid=" + strconv.Itoa(parentNodeID)
 
 	return deleteStr, nil
 }
