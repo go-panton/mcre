675ea33b6eac665854bd627dc8681f9985eb0681
Rlexsu <alex.teoh.91@gmail.com>
2016-05-12 19:29:16 +0900

    Added version service

--- files/service.go
+++ files/service.go
@@ -21,7 +21,10 @@ type BadRequestError error
 type Service interface {
 	//Download(simple) opens and reads file based on fileid
 	Download(fileid string) io.Reader
+	//Create copy the file to server and store file's metadata onto database
 	Create(string) error
+	//Version copy the file to server and create a new version based on existing file
+	Version(string, int) error
 }
 
 // NewService instantiates new download-service.
@@ -64,12 +67,14 @@ func (svc *service) Create(filename string) error {
 	// but we have to consider when error happen we want to make sure the close function always get called
 	defer fp.Close()
 
-	//1st get file name
+	//1st get file's metadata
 	fi, err := fp.Stat()
 	if err != nil {
 		return err
 	}
 
+	//fname is file's name without path before it
+	fname := fi.Name()
 	fileSize := fi.Size()
 	startDate := fi.ModTime()
 	fileDT := fi.ModTime().Format("2006-01-02") //convert to string and designated format
@@ -77,7 +82,7 @@ func (svc *service) Create(filename string) error {
 	//Get extension of fileServer
 	fileExt := path.Ext(filename)
 
-	newNode, err := nodes.NewNode(nodeID, filename, fileDT)
+	newNode, err := nodes.NewNode(nodeID, fname, fileDT)
 	if err != nil {
 		return errors.New("Node creation error: " + err.Error())
 	}
@@ -94,13 +99,14 @@ func (svc *service) Create(filename string) error {
 	dest := storageFolder + newFileName
 
 	destFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
 
 	// use defer instead of manually closing it due to cases where service failed halfway
 	defer destFile.Close()
 
-	if err != nil {
-		return err
-	}
+	defer os.Remove(dest)
 
 	_, err = io.Copy(destFile, fp)
 	if err != nil {
@@ -108,7 +114,7 @@ func (svc *service) Create(filename string) error {
 	}
 
 	//Create New Fmedia record
-	newFmedia, err := fmedias.NewFmedia(nodeID, filename, newFileName, fileExt, storageFolder, filename, int(fileSize))
+	newFmedia, err := fmedias.NewFmedia(nodeID, fname, newFileName, fileExt, storageFolder, fname, int(fileSize))
 	if err != nil {
 		return err
 	}
@@ -116,6 +122,23 @@ func (svc *service) Create(filename string) error {
 	//Create Nodelink
 	//Hardcoded the folder nodeID to 200004 and link type to FILE
 	newNodelink, err := nodes.NewNodelink(nodeID, folderNodeID, linkType)
+	if err != nil {
+		return err
+	}
+
+	//Create Fverinfo, treat it as first version since no possible way to know what version is that file
+	version := "1"
+	endDate := startDate.Add(168 * time.Hour).Format("2006-01-02")
+	verState := 1
+	remarks := "Testing Version 1"
+
+	newFverinfo, err := fmedias.NewFverinfo(nodeID, fileDT, endDate, version, verState, remarks)
+	if err != nil {
+		return err
+	}
+
+	//Create Convqueue
+	newConvqueue, err := fmedias.NewConvqueue(nodeID, time.Now().Format("2006-01-02 15:04:05"), fileExt, dest)
 	if err != nil {
 		return err
 	}
@@ -138,22 +161,18 @@ func (svc *service) Create(filename string) error {
 		}
 		fmpStrSlice = append(fmpStrSlice, fmpStr)
 	}
+	//meaning folder has no fmpolicy
+	if len(fmpStrSlice) == 0 {
+		newFmp, e := fmedias.NewFmpolicy(1, 1, 1, 0, 0, nodeID)
+		if e != nil {
+			return e
+		}
 
-	//Create Fverinfo, treat it as first version since no possible way to know what version is that file
-	version := "v1"
-	endDate := startDate.Add(168 * time.Hour).Format("2006-01-02")
-	verState := 1
-	remarks := "Testing Version 1"
-
-	newFverinfo, err := fmedias.NewFverinfo(nodeID, fileDT, endDate, version, verState, remarks)
-	if err != nil {
-		return err
-	}
-
-	//Create Convqueue
-	newConvqueue, err := fmedias.NewConvqueue(nodeID, time.Now().Format("2006-01-02 15:04:05"), fileExt, dest)
-	if err != nil {
-		return err
+		fmpStr, e := svc.fmpolicyR.GetInsertStr(newFmp)
+		if e != nil {
+			return e
+		}
+		fmpStrSlice = append(fmpStrSlice, fmpStr)
 	}
 
 	// Gather all insert statement for transaction
@@ -178,10 +197,275 @@ func (svc *service) Create(filename string) error {
 		return err
 	}
 
-	err = svc.fmediaR.CreateTx(nodeStr, fmediaStr, nlStr, fverStr, convStr, fmpStrSlice)
-	if err != nil {
+	if err = svc.fmediaR.CreateFileTx(nodeStr, fmediaStr, nlStr, fverStr, convStr, fmpStrSlice); err != nil {
 		//Remove the file from storage as the transaction has already failed
-		err := os.Remove(dest)
+		return err
+	}
+
+	if err = svc.seqR.Update("NODE", nodeID); err != nil {
+		return err
+	}
+
+	if err = svc.seqR.Update("FILENAME", fileID); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (svc *service) Version(filename string, currentFileNID int) error {
+	if filename == "" {
+		return errors.New("Filename passing in is empty!")
+	}
+
+	nodeID, err := svc.seqR.Find("NODE")
+	if err != nil {
+		return err
+	}
+	fileID, err := svc.seqR.Find("FILENAME")
+	if err != nil {
+		return err
+	}
+
+	//open the file to get the file pointer
+	fp, err := os.Open(filename)
+
+	defer fp.Close()
+
+	fi, err := fp.Stat()
+	if err != nil {
+		return err
+	}
+
+	fname := fi.Name()
+	fileSize := fi.Size()
+	startDate := fi.ModTime()
+	fileDT := fi.ModTime().Format("2006-01-02") //convert to string and designated format
+
+	//Get extension of fileServer
+	fileExt := path.Ext(filename)
+
+	newNode, err := nodes.NewNode(nodeID, fname, fileDT)
+	if err != nil {
+		return errors.New("Node creation error: " + err.Error())
+	}
+
+	const storageFolder = "D:\\PantonSys\\PTD\\ISOStorage\\1\\"
+
+	const folderNodeID = 200004
+
+	const linkType = "FILE"
+
+	newFileName := strconv.Itoa(fileID) + fileExt
+
+	dest := storageFolder + newFileName
+
+	destFile, err := os.Create(dest)
+	if err != nil {
+		return err
+	}
+
+	defer destFile.Close()
+
+	defer os.Remove(dest)
+
+	_, err = io.Copy(destFile, fp)
+	if err != nil {
+		return err
+	}
+
+	// Retrieve current file's data from database
+	currentFmedia, err := svc.fmediaR.Find(currentFileNID)
+	if err != nil {
+		return err
+	}
+
+	currentFver, err := svc.fverinfoR.Find(currentFileNID)
+	if err != nil {
+		return err
+	}
+
+	// List all version of current file using node ID and link type
+	childList, err := svc.nodelinkR.FindByParent(currentFileNID, "VER_LINKS")
+
+	//Create New Fmedia record
+	newFmedia, err := fmedias.NewFmedia(nodeID, fname, newFileName, fileExt, storageFolder, fname, int(fileSize))
+	if err != nil {
+		return err
+	}
+
+	newNodelink, err := nodes.NewNodelink(nodeID, folderNodeID, linkType)
+	if err != nil {
+		return err
+	}
+
+	// Since the database column specify version to be varchar we have to convert it back to int to increment the version number
+	// If we decide to change the value of column to int then we will be able to just increment the version number
+	version, err := strconv.Atoi(currentFver.Version)
+	if err != nil {
+		return err
+	}
+	version++
+
+	verString := strconv.Itoa(version)
+	if err != nil {
+		return err
+	}
+	endDate := startDate.Add(168 * time.Hour).Format("2006-01-02") //as of now only add 1 week
+	verState := 1
+	remarks := "Testing Version 2"
+
+	newFverinfo, err := fmedias.NewFverinfo(nodeID, fileDT, endDate, verString, verState, remarks)
+	if err != nil {
+		return err
+	}
+
+	var deleteNLSlice []string
+	var insertNLSlice []string
+
+	if len(childList) != 0 {
+		// Unlink them and relink to new file
+		for _, childNID := range childList {
+			deleteStr, e := svc.nodelinkR.GetDeleteStr(childNID, currentFileNID)
+			if e != nil {
+				return e
+			}
+
+			deleteNLSlice = append(deleteNLSlice, deleteStr)
+
+			newnl, e := nodes.NewNodelink(childNID, nodeID, "VER_LINKS")
+			if e != nil {
+				return e
+			}
+			insertStr, e := svc.nodelinkR.GetInsertStr(newnl)
+			if e != nil {
+				return e
+			}
+			insertNLSlice = append(insertNLSlice, insertStr)
+		}
+	}
+
+	// Current file is now a version of new file
+	newnl, err := nodes.NewNodelink(currentFileNID, nodeID, "VER_LINKS")
+	if err != nil {
+		return err
+	}
+
+	insertNlStr, err := svc.nodelinkR.GetInsertStr(newnl)
+	if err != nil {
+		return err
+	}
+
+	insertNLSlice = append(insertNLSlice, insertNlStr)
+
+	// Unlink current file with its parent folder, this is assuming new version will be at the same folder with current version
+	// , if not change folder nodeID to where the current version's folder node ID
+	deleteNLStr, err := svc.nodelinkR.GetDeleteStr(currentFileNID, folderNodeID)
+	if err != nil {
+		return err
+	}
+
+	deleteNLSlice = append(deleteNLSlice, deleteNLStr)
+
+	// Note to anyone seeing this: The process of unlink and relink CAN be simplified into 1 update query BUT
+	// the legacy code I refer to, remove and create a new nodelink the saying goes: "if it ain't break don't fix it!"
+
+	//Update Status of File to 0. I have no idea what 0 means since legacy code does not have 0 in FileStatus. somone screwed up and
+	//never initialized the fstatus value hence it becomes 0
+
+	//TODO: Revisit Fmedia Fstatus and Ftype, currently they are meaningless to be included, would need to discuss whether to rmeove it or retain it
+	currentFmedia.FStatus = 0
+
+	updateCurrFmStr, err := svc.fmediaR.GetUpdateStr(currentFmedia)
+	if err != nil {
+		return err
+	}
+
+	//Verstate 2 means inactive
+	currentFver.VerState = 2
+
+	if currentFver.VerState == 3 {
+		currentFver.EndDate = time.Now().Format("2006-01-02")
+	} else if currentFver.VerState == 1 {
+		currentFver.StartDate = time.Now().Format("2006-01-02")
+		currentFver.EndDate = ""
+	}
+
+	updateCurrFvStr, err := svc.fverinfoR.GetUpdateStr(currentFver)
+	if err != nil {
+		return err
+	}
+
+	fileSec, err := svc.fmpolicyR.FindUsingNodeID(currentFileNID)
+	if err != nil {
+		return err
+	}
+
+	var fmpSlice []string
+
+	for _, s := range fileSec {
+		newFmp, e := fmedias.NewFmpolicy(s.FmpDownload, s.FmpRevise, s.FmpView, s.FmpUGID, s.FmpUGType, nodeID)
+		if e != nil {
+			return e
+		}
+
+		fmpinsertStr, e := svc.fmpolicyR.GetInsertStr(newFmp)
+		if e != nil {
+			return e
+		}
+		fmpSlice = append(fmpSlice, fmpinsertStr)
+	}
+
+	//Create Convqueue
+	newConvqueue, err := fmedias.NewConvqueue(nodeID, time.Now().Format("2006-01-02 15:04:05"), fileExt, dest)
+	if err != nil {
+		return err
+	}
+
+	nodeStr, err := svc.nodeR.GetInsertStr(newNode)
+	if err != nil {
+		return err
+	}
+	fmStr, err := svc.fmediaR.GetInsertStr(newFmedia)
+	if err != nil {
+		return err
+	}
+	nlStr, err := svc.nodelinkR.GetInsertStr(newNodelink)
+	if err != nil {
+		return err
+	}
+	fvStr, err := svc.fverinfoR.GetInsertStr(newFverinfo)
+	if err != nil {
+		return err
+	}
+	convqStr, err := svc.convqueueR.GetInsertStr(newConvqueue)
+	if err != nil {
+		return err
+	}
+
+	dbStmt := fmedias.Statement{
+		CreateNodeStmt:   nodeStr,
+		CreateFmStmt:     fmStr,
+		CreateNlStmt:     nlStr,
+		CreateFvStmt:     fvStr,
+		CreateConvStmt:   convqStr,
+		CreateNLSlice:    insertNLSlice,
+		DeleteNLSlice:    deleteNLSlice,
+		UpdateCurrFmStmt: updateCurrFmStr,
+		UpdateCurrFvStmt: updateCurrFvStr,
+		CreateFmpSlice:   fmpSlice,
+	}
+
+	err = svc.fmediaR.CreateVersionTx(dbStmt)
+	if err != nil {
+		return err
+	}
+
+	if err = svc.seqR.Update("NODE", nodeID); err != nil {
+		return err
+	}
+
+	if err = svc.seqR.Update("FILENAME", fileID); err != nil {
 		return err
 	}
 
--- files/service_test.go
+++ files/service_test.go
@@ -10,7 +10,7 @@ import (
 func TestDownload(t *testing.T) {
 }
 
-func TestCreate(t *testing.T) {
+func TestCreateFile(t *testing.T) {
 	//Create a temp file in test directory
 	tempDir := os.TempDir()
 	newFile, err := os.Create(tempDir + "\\test.txt")
@@ -45,6 +45,51 @@ func TestCreate(t *testing.T) {
 	err = svc.Create(filename)
 	if err != nil {
 		t.Errorf("Error while calling create service: %v", err.Error())
+	}
+
+	err = os.Remove(tempDir + "\\test.txt")
+	if err != nil {
+		t.Errorf("Error removing test file: %v", err.Error())
+		t.FailNow() // can't remove just fail the test straight
+	}
+}
+
+func TestCreateVersion(t *testing.T) {
+	//Create a temp file in test directory
+	tempDir := os.TempDir()
+	newFile, err := os.Create(tempDir + "\\test.txt")
+	if err != nil {
+		t.Errorf("Error Creating a new file: %v", err.Error())
+		t.FailNow()
+	}
+	err = newFile.Truncate(200)
+	if err != nil {
+		t.Errorf("Error truncating the file with dummy data: %v", err.Error())
+	}
+
+	seqs := mysql.NewMockSeqRepository()
+	nodes := mysql.NewMockNodeRepository()
+	fmedias := mysql.NewMockFmediaRepository()
+	fverinfos := mysql.NewMockFverinfoRepository()
+	nls := mysql.NewMockNodelinkRepository()
+	convs := mysql.NewMockConvqueueRepository()
+	fmps := mysql.NewMockFmpolicyRepository()
+
+	svc := NewService(seqs, nodes, fmedias, nls, fverinfos, convs, fmps)
+
+	filename := newFile.Name()
+
+	currFNodeID := 200264
+
+	// close the file because the service does not need the file pointer anymore
+	err = newFile.Close()
+	if err != nil {
+		t.Errorf("Error closing file: %v", err.Error())
+		t.FailNow() //cant close file means cant delete so just fail the test straight
+	}
+	err = svc.Version(filename, currFNodeID)
+	if err != nil {
+		t.Errorf("Error while running version service: %v", err.Error())
 	}
 
 	err = os.Remove(tempDir + "\\test.txt")
